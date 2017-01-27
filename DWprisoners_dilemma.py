8246129dbec74b6a5921023376ba77b8da069327
Zombie-Harambe <452783@bsd48.org>
2017-01-27 07:59:09 -0800

    Added player name

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -446,7 +446,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 13:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Bryson'
         else:
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
