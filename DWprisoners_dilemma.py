d4b4b1cee767d4c5741e47c6e40d9d8487604a70
Me <dtking08@bsd48.org>
2017-01-27 07:54:59 -0800

    Add files via upload

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -468,7 +468,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 14:
         if getting_team_name:
-            return 'loyal vengeful occasionally greedy'
+            return 'DK! Dillon Kong! Hes the leader of the bunch...'
         else:
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
