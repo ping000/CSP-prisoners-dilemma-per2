cd229ddd4f4042c62fef515ab310ae23a04509d2
cadentb <cadenthomas23@gmail.com>
2017-01-27 07:53:14 -0800

    Added name

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -153,7 +153,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #only betrays if they were a sucker last round.
     elif player == 2:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Ping and Caden'
         else:
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
