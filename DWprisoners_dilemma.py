20abbe226775ca65e7d8f85ac0afbc0f4c58b08e
dcwillson <doug_willson@beaverton.k12.or.us>
2017-01-27 13:50:27 -0800

    Back to 17 player names...4 need to be added

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -580,7 +580,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 18:
         if getting_team_name:
-            return 'Grayson'
+            return 'loyal vengeful'
         else:
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
