c4a45aa83e939dd5824525aa6c8ac78f78420bac
Dat1AZNBanana <Micahsng@gmail.com>
2017-01-27 08:23:02 -0800

    fixed name

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -259,6 +259,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     elif player == 6:
         if getting_team_name:
             return 'backstabber'
+            return 'Dat1AZNBanana'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
