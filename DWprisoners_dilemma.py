165d30f7e8e89c1cb4f62dd9e13af0b70925f8fd
ping000 <Pchung08@bsd48.org>
2017-01-30 17:51:44 -0800

    Update DWprisoners_dilemma.py

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -194,14 +194,10 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
         if getting_team_name:
             return 'Ping and Caden'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)<5: # Collude for first 5 rounds
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return opponent_history[-1] # Do whatever opponet did last round
 
 
 
